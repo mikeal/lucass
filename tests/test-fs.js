6565fee1b10db4e98543712a4b32290e49f7c283
mikeal <mikeal.rogers@gmail.com>
2017-07-17 07:49:19 -0400

    test(fs): Adding tests for fs errors.
    Nearing 100% code coverage on the filesystem implementation. Overwriting fs globals in order to
    simulate errors.

--- tests/test-fs.js
+++ tests/test-fs.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const fs = require('fs')
+const bl = require('bl')
 const fsStore = require('../fs')
 const testdir = fs.mkdtempSync(path.join(__dirname, '.testtmp-'))
 
@@ -15,11 +16,9 @@ const failHasher = (algo, cb) => {
 
 test('fs(implementation): directory does not exist', t => {
   t.plan(1)
-  try {
+  t.throws(() => {
     fsStore('falskdjfoaisdfjoadfjodi8f9823')
-  } catch (e) {
-    t.type(e, 'Error')
-  }
+  })
 })
 
 test('fs(implementation): hash error in hash()', t => {
@@ -32,10 +31,41 @@ test('fs(implementation): hash error in hash()', t => {
 })
 
 test('fs(implementation): hash error in set()', t => {
-  t.plan(1)
+  t.plan(2)
   let store = fsStore(testdir)
   store._createHasher = failHasher
   store.set(Buffer.from('asdf'), err => {
+    t.type(err, 'Error')
+  })
+  let stream = bl()
+  store.set(stream, err => {
+    t.type(err, 'Error')
+  })
+  stream.write(Buffer.from('asdf'))
+  stream.end()
+})
+
+test('fs(implementation): filesystem errors, fs.rename()', t => {
+  t.plan(1)
+  let _rename = fs.rename
+  fs.rename = (x, y, cb) => cb(new Error('Test Error.'))
+  let store = fsStore(testdir)
+  let stream = bl()
+  store.set(stream, err => {
+    fs.rename = _rename
+    t.type(err, 'Error')
+  })
+  stream.write(Buffer.from('asdf'))
+  stream.end()
+})
+
+test('fs(implementation): filesystem errors, fs.writeFile()', t => {
+  t.plan(1)
+  let _writeFile = fs.writeFile
+  fs.writeFile = (path, buff, cb) => cb(new Error('Test Error.'))
+  let store = fsStore(testdir)
+  store.set(Buffer.from('asdf'), err => {
+    fs.writeFile = _writeFile
     t.type(err, 'Error')
   })
 })
